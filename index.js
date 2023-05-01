ac80b184b80ac19084bb5e47cd3ab2088fe9dd08
Humayun63 <humayun126224@gmail.com>
2023-05-01 22:32:31 +0600

    chefInfo response send

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 
+const chefInfo = require('./data/shefInfo.json')
+
 app.use(cors())
 
 app.get('/', (req, res)=>{
     res.send('Magic Recipe is running! YaY!')
+})
+
+app.get('/chefInfo', (req, res) =>{
+    res.send(chefInfo)
 })
 
 app.listen(port, ()=>{
