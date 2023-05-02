a42e490ba890bdec76ff999034ba1f69591db005
Humayun63 <humayun126224@gmail.com>
2023-05-02 08:59:42 +0600

    reviews data added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000
 
 const chefInfo = require('./data/shefInfo.json')
 const recipes = require('./data/recipes.json')
+const reviews = require('./data/review.json')
 
 app.use(cors())
 
@@ -26,6 +27,10 @@ app.get('/recipe/:id', (req, res) =>{
     const id = req.params.id
     const selectedRecipes = recipes.find( recipe => recipe.chef_id === id)
     res.send(selectedRecipes)
+})
+
+app.get('/reviews', (req, res)=>{
+    res.send(reviews)
 })
 
 app.listen(port, ()=>{
